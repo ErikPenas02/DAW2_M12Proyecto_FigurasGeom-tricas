3695db76700a8e47f9307c7862067082c53baec4
ErikPenyas19 <7767.joan23@fje.edu>
2024-10-24 02:36:53 +0200

    4t Commit -- Clase Rectangulo hecha (Rectangulo.php)

---
+++ clases/Rectangulo.php
@@ -0,0 +1,43 @@
+<?php
+
+class Rectangulo extends FiguraGeometrica{
+    private $lado2;    
+    // Constructor
+    function __construct($tipoFigura, $lado1, $lado2) {
+        // Llama al constructor de la clase padre (FiguraGeometrica).
+        parent::__construct($tipoFigura, $lado1);
+        $this->lado2 = $lado2;
+    }
+
+    // Getters
+    function getLado2(){
+        return $this->lado2;
+    }
+
+    // Setters
+    function setPrecio($lado2){
+        $this->precioProducto = $lado2;
+    }
+
+    //CalcularArea y Calcular Perimetro
+    function calcularArea(){
+        $areaRec = $this->lado1 * $this->lado2;
+        return $areaRec;
+    }
+
+    function calcularPerimetro(){
+        $periRec = ($this->lado1 * 2) + ($this->lado2 * 2);
+        return $periRec;
+    }
+
+
+    // toString 
+    function __toString(){
+        echo "Area: " . $this->calcularArea() . "<br> Perímetro: " . $this->calcularPerimetro();
+    }
+    // Destructor
+    function __destruct(){
+        echo "Objeto destruido";
+    }
+
+}
