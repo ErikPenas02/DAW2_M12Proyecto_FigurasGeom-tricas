640586c3cfdf036c2e3a695e1b0d34135f5fccea
ErikPenyas19 <7767.joan23@fje.edu>
2024-10-24 15:51:02 +0200

    Agragacion de Boostrap y scripts de funciones.js

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title><script src="./funciones.js" type="text/javascript"></script>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 <div class="header"></div>
-<form action="recepcion.php" method="post">
+<form action="recepcion.php" method="post" onsubmit="validarIndex(event);">
     <div>
         <div class="forma">
             <label for="" class="textoforma">Cuadrado</label><br>
             <img src="./imgs/cuadrado.webp" alt=""><br>
         </div>
-        <select name="figuras" id="figuras">
+        <select class="form-select form-select-sm" name="figuras" id="figuras" onchange="cambiarFoto();">
             <option value="cuadrao">Cuadrado</option>
             <option value="circulo">Círculo</option>
             <option value="triangulo">Triángulo</option>
